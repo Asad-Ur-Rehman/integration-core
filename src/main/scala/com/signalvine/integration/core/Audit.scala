9f90a5f331831c1465a701d2c8ed002d62ff55d7
shahbazshub <shahbazshueb@gmail.com>
2017-03-23 03:45:41 +0500

    Add Result and Audit to core

---
+++ src/main/scala/com/signalvine/integration/core/Audit.scala
@@ -0,0 +1,21 @@
+package com.signalvine.integration.core
+
+case class Audit(auditId: UUID[Audit],
+                 syncJobId: UUID[SyncJob],
+                 event: EventType,
+                 by: String,
+                 time: DateTime,
+                 jobConfiguration: JobConfiguration,
+                 result: Option[Result]
+                )
+
+sealed trait  EventType{
+  def value: String
+}
+
+case object IntegrationStarted extends EventType{val value = "IntegrationStarted"}
+case object IntegrationSucceeded extends EventType{val value = "IntegrationSucceeded"}
+case object IntegrationFailed extends EventType{val value = "IntegrationFailed"}
+case object ConfigCreated extends EventType{val value = "ConfigCreated"}
+case object ConfigDeleted extends EventType{val value = "ConfigDeleted"}
+case object ConfigUpdated extends EventType{val value = "ConfigUpdated"}
