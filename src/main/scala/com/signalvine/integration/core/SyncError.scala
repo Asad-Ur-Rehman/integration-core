fa8d7e3fb492e71d9f29bfea8e7309ed8a49db7f
shahbazshueb-10p <shahbaz.ali@tenpearls.com>
2017-06-02 20:24:39 +0500

    Update SyncError according to API response

--- src/main/scala/com/signalvine/integration/core/SyncError.scala
+++ src/main/scala/com/signalvine/integration/core/SyncError.scala
@@ -5,27 +5,37 @@ import play.api.libs.functional.syntax._
 
 sealed trait IntegrationError {
   val message: String
-  val details: String
+  val details: Option[String]
 }
 
 sealed trait Participant
 
 case class SyncError(
-                      participantId: UUID[Participant],
-                      message: String,
-                      details: String
-                    ) extends IntegrationError
+    participantId: Option[UUID[Participant]],
+    colName: Option[String],
+    row: Option[Int],
+    column: Option[Int],
+    message: String,
+    details: Option[String]
+) extends IntegrationError
 
 object SyncError {
+
   implicit val syncErrorReads: Reads[SyncError] =(
-    (__ \ 'participantId).read[UUID[Participant]] and
-    (__ \ 'message).read[String] and
-    (__ \ 'details).read[String]
+    (__ \ 'participantId).readNullable[UUID[Participant]] and
+    (__ \ 'colname).readNullable[String] and
+    (__ \ 'row).readNullable[Int] and
+    (__ \ 'col).readNullable[Int] and
+    (__ \ 'msg).read[String] and
+    (__ \ 'details).readNullable[String]
   ) (SyncError.apply _)
 
   implicit val syncErrorWrites: Writes[SyncError] = (
-    (__ \ 'participantId).write[UUID[Participant]] and
-    (__ \ 'message).write[String] and
-    (__ \ 'details).write[String]
+    (__ \ 'participantId).writeNullable[UUID[Participant]] and
+    (__ \ 'colname).writeNullable[String] and
+    (__ \ 'row).writeNullable[Int] and
+    (__ \ 'col).writeNullable[Int] and
+    (__ \ 'msg).write[String] and
+    (__ \ 'details).writeNullable[String]
   ) (unlift(SyncError.unapply))
 }
--- src/test/scala/com/signalvine/test/integration/core/SyncErrorSpec.scala
+++ src/test/scala/com/signalvine/test/integration/core/SyncErrorSpec.scala
@@ -10,28 +10,37 @@ class SyncErrorSpec extends Specification with JsonMatchers {
     val participantId = UUID.fromRaw[Participant](UUID.genRaw())
     val message = "This is an error message"
     val details = "Some details"
+    val colname = "test"
+    val row = 1
 
     def anObjectWith(fields: (String, Matcher[JsonType])*): Matcher[String] =
       fields.map { case (name, matcher) => /(name).andHave(matcher): Matcher[String] }.reduceLeft(_ and _)
 
     "serialize into JSON with all fields provided" >> {
-      val o = new SyncError(participantId, message, details)
-      val json = Json.toJson(o).toString
+      val o = new SyncError(Some(participantId), Some(colname), Some(row), None, message, Some(details))
+      val json = Json.toJson(o)
+      val jsonString = json.toString
 
-      json must anObjectWith(
+      jsonString must anObjectWith(
         "participantId" -> participantId.toString,
-        "message" -> message,
-        "details" -> details
+        "msg" -> message,
+        "details" -> details,
+        "row" -> 1,
+        "colname" -> "test"
       )
+      (json \ "col").asOpt[Int] mustEqual None
     }
 
     "deserialize into object with all fields provided" >> {
-      val json = Json.parse(s"""{"participantId": "${participantId.toString}", "message": "$message", "details": "$details"}""")
+      val json = Json.parse(s"""{"participantId": "${participantId.toString}", "msg": "$message", "details": "$details", "colname":"$colname", "row":$row}""")
       val o = Json.fromJson[SyncError](json).get
 
-      o.participantId mustEqual participantId
+      o.participantId.get mustEqual participantId
+      o.colName.get mustEqual colname
+      o.column mustEqual None
+      o.row mustEqual Some(1)
       o.message mustEqual message
-      o.details mustEqual details
+      o.details.get mustEqual details
     }
   }
 }
